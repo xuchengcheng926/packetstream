6e3c87d265854630ac537a8b9188633bec21f430
chengscc <44265641+xuchengcheng926@users.noreply.github.com>
2021-09-18 23:35:01 +0800

    Update pack2fly.sh

--- pack2fly.sh
+++ pack2fly.sh
@@ -3,7 +3,7 @@
 export COMPOSE_HTTP_TIMEOUT=500
 wget https://raw.githubusercontent.com/xuchengcheng926/packetstream/main/docker-compose.yaml -O docker-compose.yaml  
 
-sed -i "s/CID=.*/CID=$1/g" docker-compose.yml
+sed -i "s/CID=.*/CID=$1/g" docker-compose.yaml
 sed -i "s/http_proxy=.*/http_proxy=$2/g" docker-compose.yaml
 sed -i "s/https_proxy=.*/https_proxy=$2/g" docker-compose.yaml
 docker-compose up -d --no-recreate
