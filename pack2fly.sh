dc4df48209cc06c2eee130cfe085d821a8ee9125
chengscc <44265641+xuchengcheng926@users.noreply.github.com>
2021-09-18 23:10:37 +0800

    Update pack2fly.sh

--- pack2fly.sh
+++ pack2fly.sh
@@ -3,7 +3,7 @@
 
 wget https://raw.githubusercontent.com/xuchengcheng926/packetstream/main/docker-compose.yaml -O docker-compose.yaml  
 
-sed -i "s/CID=.*/CID=$CID/g" docker-compose.yml
-sed -i "s/http_proxy=.*/http_proxy=$http_proxy/g" docker-compose.yml
-sed -i "s/https_proxy=.*/https_proxy=$https_proxy/g" docker-compose.yml
+sed -i "s/CID=.*/CID=$1/g" docker-compose.yml
+sed -i "s/http_proxy=.*/http_proxy=$2/g" docker-compose.yml
+sed -i "s/https_proxy=.*/https_proxy=$2/g" docker-compose.yml
 docker-compose up -d --no-recreate
