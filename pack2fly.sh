edd102bb50900bf0e5e5129dba3e272249c6d65e
chengscc <44265641+xuchengcheng926@users.noreply.github.com>
2021-09-18 23:30:29 +0800

    Update pack2fly.sh

--- pack2fly.sh
+++ pack2fly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+export COMPOSE_HTTP_TIMEOUT=500
 wget https://raw.githubusercontent.com/xuchengcheng926/packetstream/main/docker-compose.yaml -O docker-compose.yaml  
 
 sed -i "s/CID=.*/CID=$1/g" docker-compose.yml
